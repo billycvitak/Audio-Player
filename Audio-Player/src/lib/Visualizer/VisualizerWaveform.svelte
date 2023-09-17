9d7aa7d9748c0f12d5c614e393a3aa4333759ba0
billycvitak <145053822+billycvitak@users.noreply.github.com>
2023-09-17 11:27:44 +0200

    Delete Audio-Player directory

--- Audio-Player/src/lib/Visualizer/VisualizerWaveform.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	
-</script>
-
-<div class="w-full rotate-90 transform-gpu h-96 pb-2 pt-2">
-	test
-</div>
-
--- Audio-Player/src/lib/store.js
+++
@@ -1,5 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const filterValueStore = writable('hue-rotate-0');
-export const visualizerStore = writable('Bar');
-
